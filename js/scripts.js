3ed5e995ff9e489daa71dbb5b4a1d19a8ef76769
Calla Rudolph <callarudolph@gmail.com>
2017-06-15 18:31:27 -0700

    create deposit/withdraw prototypes and separate #activity submit button

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,23 @@ function Account(name, initial) {
   this.fullName = name;
   this.initial = initial;
   this.balance = initial;
-  this.deposit;
-  this.withdraw;
 }
 
 Account.prototype.addToBalance = function(depositParameter) {
   this.deposit = depositParameter;
-  this.balance = this.balance + this.deposit;
-  console.log(this.balance);
-  return this.balance + this.deposit
+  this.balance += this.deposit;
+  return this.balance
+}
+
+Account.prototype.subtractFromBalance = function(withdrawalParameter) {
+  this.withdrawal = withdrawalParameter;
+  this.balance -= this.withdrawal;
+  return this.balance
 }
 
 function resetFields() {
   $("input#new-name").val("");
-  // $("input#initial-deposit").val("0");
+  $("input#initial-deposit").val("0");
   $("input#deposit").val("0");
   $("input#withdraw").val("0");
 }
@@ -27,24 +30,25 @@ $(document).ready(function() {
   $("#new-account").submit(function(event) {
     event.preventDefault();
 
-  var fullName = $("input#new-name").val();
-  var initial = parseInt($("input#initial-deposit").val());
-  var deposit = parseInt($("input#deposit").val());
-  var withdraw = parseInt($("input#withdraw").val());
+    var fullName = $("input#new-name").val();
+    var initial = parseInt($("input#initial-deposit").val());
+    bankAccount = new Account(fullName, initial);
+    $(".panel").show();
+    $(".name").text(bankAccount.fullName);
+    $(".money").text("$" + bankAccount.balance);
+    $("#new-account").hide();
+    $("#activity").show();
+  });
 
+  $("#activity").submit(function(event) {
+    event.preventDefault();
+    var deposit = parseInt($("input#deposit").val());
+    var withdraw = parseInt($("input#withdraw").val());
 
-  var bankAccount = new Account(fullName, initial);
-
-  var newDeposit = bankAccount.addToBalance(deposit);
-
-  console.log(bankAccount);
-
-  // var accountBalance = new Balance(deposit, withdraw);
-  $(".panel").show();
-  $(".name").text(bankAccount.fullName);
-  // $(".money").text("$" + bankAccount.Balance());
-
-  resetFields();
-
+    var newDeposit = bankAccount.addToBalance(deposit);
+    var newWithdrawal = bankAccount.subtractFromBalance(withdraw);
+    $(".money").text("$" + newDeposit);
+    $(".money").text("$" + newWithdrawal);
+    resetFields();
   });
 });
